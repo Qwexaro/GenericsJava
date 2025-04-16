2174faae7dd07423ffe9120449833e8895bab0d6
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-16 12:33:16 +0300

    Main

--- src/main/java/TwoCellsBox.java
+++ src/main/java/TwoCellsBox.java
@@ -1,12 +1,18 @@
+import java.util.List;
+
 public class TwoCellsBox<T, S> extends Box {
+
     private S secondItem;
+    private List<Garbage> allTheGarbage;
 
     public void putItems(T item, S secondItem) {
         super.putItem(item);
         this.secondItem = secondItem;
+        allTheGarbage.add((Garbage) item);
+        allTheGarbage.add((Garbage) secondItem);
     }
 
-    public S getSecondItem() {
+    public S getSecondItems() {
         return secondItem;
     }
 }
