47548693705a8983f8b806c59b6a4b46f652a3e5
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-16 12:06:58 +0300

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ public class Main {
         Glass firstPartGlass = new Glass("Армированное");
         Glass secondPartGlass = new Glass("Сталинит");
 
+        Paper firstPartPaper = new Paper("Газетная бумага");
+
         Box<Plastic> boxForPlastic = new Box<>();
         boxForPlastic.putItem(firstPartPlastic);
         boxForPlastic.putItem(secondPartPlastic);
@@ -16,5 +18,8 @@ public class Main {
         boxForGlass.putItem(secondPartGlass);
         System.out.println("Вторая партия мусора: " + boxForGlass.getItem().display());
 
+        Box<Paper> boxForPaper = new Box<>();
+        boxForPaper.putItem(firstPartPaper);
+        System.out.println("Третья партия мусора " + boxForPaper.getItem().display());
     }
 }
